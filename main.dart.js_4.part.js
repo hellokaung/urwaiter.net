5e4a63f73383e31ea51196616e178818f80234b5
Cowboy <kaungmyatko420@gmail.com>
2026-03-10 02:33:21 +0700

    add animation

--- main.dart.js_4.part.js
+++
@@ -1,8 +0,0 @@
-((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A,B,C
-A=c[0]
-B=c[2]
-C=c[12]
-var z=a.updateTypes([]);(function constants(){C.hj=new A.h(1,0.2196078431372549,0.5568627450980392,0.23529411764705882,B.c)
-C.j3=new A.h(1,0.10588235294117647,0.3686274509803922,0.12549019607843137,B.c)})()};
-(a=>{a["1kpLqfDY3VtX2UmO6sl4ZDBix18="]=a.current})($__dart_deferred_initializers__);
